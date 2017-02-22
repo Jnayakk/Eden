59fba3befa42932bf3caac19f58a77ad8bc9c6cd
JNayakk <mrelitetm@gmail.com>
2017-02-22 16:10:51 -0500

    Added main activity and created more strings

---
+++ app/src/main/java/backend/Carbon.java
@@ -0,0 +1,12 @@
+package backend;
+
+/**
+ * Created by Jay on 2017-02-22.
+ */
+
+public class Carbon {
+    public static void main(String[] args) {
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Hello, World");
+    }
+}
