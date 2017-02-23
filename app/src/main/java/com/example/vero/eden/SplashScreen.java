0efd4a1bfaad992ea64a6007801b455d40e56b0a
JNayakk <mrelitetm@gmail.com>
2017-02-22 20:19:03 -0500

    Added more activities, designed the main activity UI

--- app/src/main/java/com/example/vero/eden/SplashScreen.java
+++ app/src/main/java/com/example/vero/eden/SplashScreen.java
@@ -1,7 +1,13 @@
 package com.example.vero.eden;
 
+import android.app.ActionBar;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -9,5 +15,13 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
     }
+
+    public void mainactivity(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
 }
